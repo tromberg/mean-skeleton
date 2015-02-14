ac5ee025bb29a21c99a9bdb5b8eccb78cd545bf8
timr <tim.romberg@gmail.com>
2015-02-14 20:33:46 +0100

    refactored with Google Maps

--- public/controllers/droovle-app.js
+++ public/controllers/droovle-app.js
@@ -10,6 +10,7 @@
         this.markers = [];
         this.step = 1;
         this.places = [];
+        this.placesDirty = false;
         this.initOnlySearchBox();
     };
 
@@ -24,6 +25,8 @@
 
 
     CreateDroovleCtrl.prototype.updateMapMarkersFromPlaces = function() {
+        if (!this.placesDirty) return;
+
         var places = this.places;
         if (places.length == 0) {
             return;
@@ -62,6 +65,21 @@
 
         }
 
+        // ensure minimum bounds size
+        var ne = bounds.getNorthEast();
+        var sw = bounds.getSouthWest();
+        var ewExtendSpan = (0.01 - ne.lng() + sw.lng()) / 2;
+        if (ewExtendSpan > 0) {
+            bounds.extend(new google.maps.LatLng(ne.lat(), ne.lng() + ewExtendSpan));
+            bounds.extend(new google.maps.LatLng(sw.lat(), sw.lng() - ewExtendSpan));
+        }
+
+        var nsExtendSpan = (0.005 - ne.lat() + sw.lat()) / 2;
+        if (nsExtendSpan > 0) {
+            bounds.extend(new google.maps.LatLng(ne.lat() + nsExtendSpan, ne.lng()));
+            bounds.extend(new google.maps.LatLng(sw.lat() - nsExtendSpan, sw.lng()));
+        }
+
         map.fitBounds(bounds);
     }
 
@@ -82,15 +100,13 @@
         // pick list. Retrieve the matching places for that item.
         google.maps.event.addListener(searchBox, 'places_changed', function() {
             _this.places = searchBox.getPlaces();
-
+            _this.placesDirty = true;
             _this.updateMapMarkersFromPlaces();
 
         });
         // [END region_getplaces]
 
-        if (this.places.length > 0) {
-            this.updateMapMarkersFromPlaces();
-        }
+        this.updateMapMarkersFromPlaces();
 
     }
 
@@ -104,6 +120,11 @@
         var _this = this;
         google.maps.event.addListener(searchBox, 'places_changed', function() {
             _this.places = searchBox.getPlaces();
+            _this.placesDirty = true;
+            // if user presses enter on dropdown list in step1, this event is usually called when
+            // we are already in step2, hence during the previous call to initialize, the
+            // map was not yet updated
+            if (_this.step === 2) _this.updateMapMarkersFromPlaces();
 
         });
     }
